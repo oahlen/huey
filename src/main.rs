3ce19647632a6b6838263dde7fdd8ee17af0392f
Oscar Ahlén <github@ahlen.dev>
2023-05-07 15:36:14 +0200

    Make output directory completely configurable

--- src/main.rs
+++ src/main.rs
@@ -52,12 +52,12 @@ fn get_root_dir(output: Option<String>) -> Result<String, anyhow::Error> {
 }
 
 fn setup_directories(output: &str, name: &str) -> Result<(), anyhow::Error> {
-    match fs::create_dir_all(format!("{output}/{name}/lua/{name}")) {
+    match fs::create_dir_all(format!("{output}/lua/{name}")) {
         Ok(_) => {}
         Err(error) => return Err(error.into()),
     };
 
-    match fs::create_dir_all(format!("{output}/{name}/colors")) {
+    match fs::create_dir_all(format!("{output}/colors")) {
         Ok(_) => {}
         Err(error) => return Err(error.into()),
     }
@@ -67,8 +67,8 @@ fn setup_directories(output: &str, name: &str) -> Result<(), anyhow::Error> {
 
 fn generate_vim_colors_file(output: &str, name: &str) -> Result<(), anyhow::Error> {
     match fs::write(
-        format!("{output}/{name}/colors/{name}.lua"),
-        format!("require(\"{name}\").init({{}})\n"),
+        format!("{output}/colors/{name}.lua"),
+        format!("require(\"{name}\").init()\n"),
     ) {
         Ok(_) => Ok(()),
         Err(error) => Err(error.into()),
@@ -79,7 +79,7 @@ fn generate_init(output: &str, theme: Theme) -> Result<(), anyhow::Error> {
     let name = &theme.name;
     let background = &theme.background;
 
-    let file = File::create(format!("{output}/{name}/lua/{name}/init.lua"))?;
+    let file = File::create(format!("{output}/lua/{name}/init.lua"))?;
     let mut file = LineWriter::new(file);
 
     file.write_all(
