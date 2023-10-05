da5d1391d6ab5f7c1de64c82d5988164df488dc6
Oscar Ahlén <github@ahlen.dev>
2023-10-05 20:32:32 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,49 +3,79 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     rust-overlay,
+    crane,
+    flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [
-        (import rust-overlay)
-        (self: super: {
-          rustToolchain = let
-            rust = super.rust-bin;
-          in
-            if builtins.pathExists ./rust-toolchain.toml
-            then rust.fromRustupToolchainFile ./rust-toolchain.toml
-            else if builtins.pathExists ./rust-toolchain
-            then rust.fromRustupToolchainFile ./rust-toolchain
-            else rust.stable.latest.default;
-        })
-      ];
-
+      overlays = [(import rust-overlay)];
       pkgs = import nixpkgs {inherit system overlays;};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          bacon
-          cargo-deny
-          cargo-edit
-          cargo-watch
-          openssl
-          pkg-config
-          rust-analyzer
-          rustToolchain
-        ];
 
-        shellHook = ''
-          Using rust version:
-          ${pkgs.rustToolchain}/bin/cargo --version
-        '';
+      rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+      src = craneLib.cleanCargoSource ./.;
+
+      nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
+
+      buildInputs = with pkgs; [openssl sqlite];
+
+      commonArgs = {
+        inherit src buildInputs nativeBuildInputs;
       };
-    });
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      bin = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+        });
+
+      dockerImage = pkgs.dockerTools.buildLayeredImage {
+        name = "huey";
+        tag = "latest";
+
+        contents = [bin];
+
+        config = {
+          Cmd = ["${bin}/bin/huey"];
+        };
+      };
+    in
+      with pkgs; {
+        packages = {
+          inherit bin dockerImage;
+          default = bin;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [bin];
+          buildInputs = [rust-analyzer];
+        };
+      });
 }
